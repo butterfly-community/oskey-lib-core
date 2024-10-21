2655c7393b99691a892b0f691243fc4362364e60
Septimus Shepherd <septimus.shepherd@gmail.com>
2024-10-21 18:51:13 +0800

    fix(bip39): re-export bip39

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #[cfg(feature = "bip32")]
 pub use ohw_algorithm_bip32 as bip32;
 
+#[cfg(feature = "bip39")]
+pub use ohw_algorithm_bip39 as bip39;
+
 #[cfg(feature = "bip44")]
 pub use ohw_algorithm_bip44 as bip44;
